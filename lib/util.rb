6a01d438ca30895774b710460774509463ed3bb7
chengenbao <genbao.chen@gmail.com>
2014-11-10 21:02:30 +0800

    initial import

---
+++ lib/util.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+###################################################################
+#
+# Filename: util.rb
+#
+# 工具模块，提供各种通用功能
+# 
+# Author: chengenbao
+# Email: genbao.chen@gmail.com
+#
+###################################################################
+
+
+module Util
+  require './util/http_requester'
+end
